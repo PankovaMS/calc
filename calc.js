ced0ae7ceb6f51bff326912639b888c1db136f58
PankovaMS <lmargojayl@gmail.com>
2025-04-16 11:22:24 +0300

    добавлено явное преобразование типов, number

--- calc.js
+++ calc.js
@@ -1,43 +1,34 @@
-// Формула сложных процентов:
-// A = P * (1 + r/100/12)^(termInMonths)
-// где:
-//  - P – начальная сумма вклада (principal),
-//  - r – годовая процентная ставка (interestRate),
-//  - termInMonths – срок вклада в месяцах.
- 
-// Рассчитываем итоговую сумму A, затем общий доход (totalIncome = A - P)
-// и средний доход в месяц (monthlyIncome = (A - P) / termInMonths).
-
 let totalIncome = 0;
 let monthlyIncome = 0;
 
 function calc () {
-    let month = parseInt(document.querySelector('#months').value);
-    let rate = parseInt(document.querySelector('#rate').value);
-    let sum = parseInt(document.querySelector('#sum').value);
+    let month = Number(document.querySelector('#months').value, 10);
+    let rate  = Number(document.querySelector('#rate').value, 10);
+    let sum   = Number(document.querySelector('#sum').value, 10);
 // валидация
-    if (isNaN(month) || month < 1 || month > 120 || !Number.isInteger(month)) {
-        alert("Срок вклада должен быть от 1 до 120 месяцев.");
-        return;
-      }
+if (isNaN(month) || month < 1 || month > 120) {
+    alert("Срок вклада должен быть от 1 до 120 месяцев.");
+    return;
+  }
 
-    if (isNaN(rate) || rate < 1 || rate > 40 || !Number.isInteger(rate)) {
-        alert("Процентная ставка должна быть от 1 до 40 %.");
-        return;
-    }  
+  if (isNaN(rate) || rate < 1 || rate > 40) {
+    alert("Процентная ставка должна быть от 1 до 40 %.");
+    return;
+  }  
 
-    if (isNaN(sum) || sum < 1000 || sum > 10000000 || !Number.isInteger(sum)) {
-        alert("Сумма вклада должна быть от 1000 до 10000000.");
-        return;
-    }
+  if (isNaN(sum) || sum < 1000 || sum > 10000000) {
+    alert("Сумма вклада должна быть от 1000 до 10000000.");
+    return;
+  }
+
 
     if (sum > 0) {
-       monthlyIncome = sum * rate / 100 / 12;
-       totalIncome = sum * (1 + rate / 100 / 12) ** month;
+        monthlyIncome = sum * rate / 100 / 12;
+        totalIncome = sum * Math.pow((1 + rate / 100 / 12), month);
     }
     if (sum >= 1400000) {
         alert('Осторожно: вклад не застрахован государством!');
-    }
+      }
     console.log(monthlyIncome, totalIncome);
 
     document.querySelector('#totalIncome').innerHTML = `${totalIncome.toFixed(2)} рублей`;
